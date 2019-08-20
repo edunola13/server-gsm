e8b4b2a5f1dc308976be5ca2f91e1d0dfde09b63
Eduardo Nola <eduardo@bitpagos.co>
2019-08-20 16:45:47 -0300

    Change base model

--- apps/devices/models.py
+++ apps/devices/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 from .constants import (STATUS_CHOICES, STATUS_INI, CHIP_STATUS_CHOICES, CHIP_STATUS_FREE,
                         LOG_TYPE_CHOICES, LOG_STATUS_CHOICES, ORIGIN_CHOICES, RULE_TYPE_CHOICES, RULE_TYPE_DEFAULT, RULE_FROM_TYPE_CHOICES,
-                        RULE_TO_TYPE_CHOICES, RULE_STATUS_CHOICES, RULE_STATUS_OK)
+                        RULE_TO_TYPE_CHOICES)
 
 
 class Device (models.Model):
@@ -15,6 +15,7 @@ class Device (models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=STATUS_INI)
     chip_status = models.CharField(max_length=10, choices=CHIP_STATUS_CHOICES, default=CHIP_STATUS_FREE)
 
+    index_sms = models.IntegerField(default=1)
     channel_i2c = models.CharField(max_length=10)
     last_connection = models.DateTimeField(null=True)
     enabled = models.BooleanField(default=True)
@@ -24,7 +25,7 @@ class Device (models.Model):
 
 
 class LogDevice (models.Model):
-    status = models.CharField(max_length=10, choices=LOG_STATUS_CHOICES)
+    # status = models.CharField(max_length=10, choices=LOG_STATUS_CHOICES)
     log_type = models.CharField(max_length=10, choices=LOG_TYPE_CHOICES)
     number = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
@@ -32,6 +33,30 @@ class LogDevice (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     device = models.ForeignKey(Device, on_delete=models.PROTECT,)
+
+
+class Rule (models.Model):
+    name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=10, choices=ORIGIN_CHOICES)
+    rule_type = models.CharField(max_length=10, choices=RULE_TYPE_CHOICES, default=RULE_TYPE_DEFAULT)
+    from_type = models.CharField(max_length=10, choices=RULE_FROM_TYPE_CHOICES, null=True)
+    from_number = models.CharField(max_length=255, null=True)
+    to_type = models.CharField(max_length=10, choices=RULE_TO_TYPE_CHOICES, null=True)
+    to_number = models.CharField(max_length=255, null=True)
+    enabled = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    device = models.ForeignKey(Device, related_name='rules', on_delete=models.PROTECT,)  # Puede no tener device
+
+
+class RuleInstance (models.Model):
+    description = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    log_device = models.ForeignKey(LogDevice, on_delete=models.PROTECT,)
+    rule = models.ForeignKey(Rule, on_delete=models.PROTECT,)
 
 
 class LogAction (models.Model):
@@ -44,27 +69,4 @@ class LogAction (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     device = models.ForeignKey(Device, on_delete=models.PROTECT,)
-
-
-class Rule (models.Model):
-    name = models.CharField(max_length=100)
-    rule_type = models.CharField(max_length=10, choices=RULE_TYPE_CHOICES, default=RULE_TYPE_DEFAULT)
-    from_type = models.CharField(max_length=10, choices=RULE_FROM_TYPE_CHOICES, null=True)
-    from_number = models.CharField(max_length=255, null=True)
-    to_type = models.CharField(max_length=10, choices=RULE_TO_TYPE_CHOICES, null=True)
-    to_number = models.CharField(max_length=255, null=True)
-    enabled = models.BooleanField(default=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    device = models.ForeignKey(Device, related_name='rules', on_delete=models.PROTECT,)
-
-
-class RuleInstance (models.Model):
-    status = models.CharField(max_length=10, choices=RULE_STATUS_CHOICES, default=RULE_STATUS_OK)
-    description = models.TextField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    log = models.ForeignKey(LogDevice, on_delete=models.PROTECT,)
-    rule = models.ForeignKey(Rule, on_delete=models.PROTECT,)
+    rule_instance = models.ForeignKey(RuleInstance, on_delete=models.PROTECT,)
