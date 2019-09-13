765609fff8a6f90ae62a17194359382bb2d65841
Eduardo Nola <eduardo@bitpagos.co>
2019-09-13 15:17:42 -0300

    Change redlock library

--- server/redis_lock.py
+++ server/redis_lock.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 
-from redlock import Redlock
+from redlock import RedLock
 
 
 class RedisLockFail(Exception):
@@ -17,14 +17,20 @@ class Lock():
         self.ttl = ttl
         retry = retry if retry else settings.REDIS_BLOCKER_RETRY
         delay = delay if delay else settings.REDIS_BLOCKER_DELAY
-        self.redis = Redlock(settings.REDIS_BLOCKER, retry, delay)
+        self.redis = RedLock(key, settings.REDIS_BLOCKER, retry, delay, ttl)
 
     def __enter__(self):
-        self.lock = self.redis.lock(self.key, self.ttl)
-        if self.lock is False:
-            raise RedisLockFail("Can't get block")
-        return self.lock
+        self.redis.acquire()
 
     def __exit__(self, type, value, traceback):
-        if self.lock:
-            self.redis.unlock(self.lock)
+        self.redis.release()
+
+    # def __enter__(self):
+    #     self.lock = self.redis.lock(self.key, self.ttl)
+    #     if self.lock is False:
+    #         raise RedisLockFail("Can't get block")
+    #     return self.lock
+
+    # def __exit__(self, type, value, traceback):
+    #     if self.lock:
+    #         self.redis.unlock(self.lock)
