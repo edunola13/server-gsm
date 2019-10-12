5b2ede7f2c430e5b6909ae697090bca15169497d
Eduardo Nola <edunola13@hotmail.com>
2019-10-11 22:13:58 -0300

    delete sms

--- apps/devices/models.py
+++ apps/devices/models.py
@@ -95,7 +95,7 @@ class Device (models.Model):
             self.index_sms = new_index
             self.save()
 
-        if new_index > 11:
+        if self.index_sms > 11:
             self.launch_delete_sms(10)
 
     def check_new_sms(self):
